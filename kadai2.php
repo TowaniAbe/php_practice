824073b4c58401a2917f56beb843bd3c163e8351
EC2 Default User <ec2-user@ip-172-31-17-21.us-east-2.compute.internal>
2020-07-29 02:48:48 +0000

    laravel02

---
+++ kadai2.php
@@ -0,0 +1,18 @@
+<?php
+$name = "abe_towani";
+if ($name == "yamada_tarou") {
+   echo "あなたの名前ではありません";
+ } else {
+   echo "わたしはabe towaniです" . "\n";
+ }
+ 
+ 
+  $total = 0;
+ echo $total;
+ 
+ for ($i = 0; $i <= 10000; $i++) {
+    $total += $i;
+ }
+ echo $total . "\n";
+ 
+ 
