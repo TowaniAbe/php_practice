c6b65d6ce64a7522c2d5c35a0d1c4cad51178108
EC2 Default User <ec2-user@ip-172-31-17-21.us-east-2.compute.internal>
2020-08-01 06:28:49 +0000

    laravel02

--- kadai2.php
+++ kadai2.php
@@ -1,4 +1,6 @@
 <?php
+
+// kadai1
 $name = "abe_towani";
 if ($name == "yamada_tarou") {
    echo "あなたの名前ではありません";
@@ -6,13 +8,33 @@ if ($name == "yamada_tarou") {
    echo "わたしはabe towaniです" . "\n";
  }
  
- 
+// kadai2
   $total = 0;
- echo $total;
  
  for ($i = 0; $i <= 10000; $i++) {
     $total += $i;
  }
  echo $total . "\n";
  
+ // kadai3
+ $fruits = array("orange","apple","pear","grape","strawberry");
  
+ foreach($fruits as $fruits){
+     echo "要素は" . $fruits;
+     echo "\n";
+ }
+ 
+ //kadai4
+ // for文の始めの値を定義する
+$start = 5;
+/* for文の終わりの値を定義する */
+$end = 105;
+
+for($i = $start; $i < $end; $i++){
+
+  // 5で割り切れたら{}内を実行する
+  if($i % 5 == 0){
+    echo $i;
+    echo "\n";
+  }
+}
